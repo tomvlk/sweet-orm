af3d7f75f0d7898ce009861b99499fb5f1243260
Tom Valk <tomvalk@gmail.com>
2016-01-09 15:53:51 +0100

    Add abstract Entity class, will be used to extend all developers entities!

---
+++ src/SweatORM/Entity.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Entity Abstract Class
+ *
+ * @author     Tom Valk <tomvalk@lt-box.info>
+ * @copyright  2016 Tom Valk
+ */
+
+namespace SweatORM;
+
+/**
+ * Entity. Extend this class for all the functionality you get with the ORM
+ *
+ * @package SweatORM
+ */
+abstract class Entity
+{
+    /**
+     * @var boolean Saved state
+     */
+    public $_saved = false;
+
+    /**
+     * @var int|string Primary Key value
+     */
+    private $_id;
+
+
+}
---
+++ src/SweatORM/EntityManager.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Entity Manager
+ *
+ * @author     Tom Valk <tomvalk@lt-box.info>
+ * @copyright  2016 Tom Valk
+ */
+
+namespace SweatORM;
+
+
+abstract class EntityManager
+{
+    //private
+}
