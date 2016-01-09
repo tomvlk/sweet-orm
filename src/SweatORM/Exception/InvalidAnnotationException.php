c22429b51a79893aa281bfa45bb3acd0f6c7eed7
Tom Valk <tomvalk@gmail.com>
2016-01-09 16:36:16 +0100

    Adding exception classes.

---
+++ src/SweatORM/Exception/InvalidAnnotationException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Invalid Annotation Exception
+ *
+ * @author     Tom Valk <tomvalk@lt-box.info>
+ * @copyright  2016 Tom Valk
+ */
+
+namespace SweatORM\Exception;
+
+/**
+ * Invalid Annotation Exception
+ *
+ * @package SweatORM\Exception
+ */
+class InvalidAnnotationException extends ORMException
+{
+}
